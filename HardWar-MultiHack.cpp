6e3b06db84678e5f8608b91f81ceaa25f5157c6b
Paranoidopolis <jrose970@mycod.us>
2022-07-27 01:51:38 -0700

    Update

--- HardWar-MultiHack.cpp
+++ HardWar-MultiHack.cpp
@@ -15,18 +15,18 @@
 int main()
 {
 	// Memory offsets
-	std::vector<unsigned int> playerCashOffsets = {0x3C};
-	std::vector<unsigned int> playerStatusOffsets = {0x58};
-	std::vector<unsigned int> playerShieldOffsets = {0x30, 0x294};
-	std::vector<unsigned int> playerThrustOffsets = {0x30, 0x438};
-	std::vector<unsigned int> playerStallWarningLvlOffsets = {0x30, 0x390};
-	std::vector<unsigned int> currentTargetTypeOffsets = {0x30, 0x1EC};
-	std::vector<unsigned int> currentTargetStructureDmgOffsets = {0x30, 0x1F0, 0x29C};
+	std::vector<unsigned int> cashOffsets = {0x3C};
+	std::vector<unsigned int> statusOffsets = {0x58};
+	std::vector<unsigned int> shieldOffsets = {0x30, 0x294};
+	std::vector<unsigned int> thrustOffsets = {0x30, 0x438};
+	std::vector<unsigned int> stallWarningLvlOffsets = {0x30, 0x390};
+	std::vector<unsigned int> targetTypeOffsets = {0x30, 0x1EC};
+	std::vector<unsigned int> targetStructureDmgOffsets = {0x30, 0x1F0, 0x29C};
 
 	// Game constants
-	const int MAX_SHIELDS = 16384;
-	const int MAX_DMG = 16384;
-	const int MAX_THRUST = 99999;
+	const DWORD MAX_SHIELDS = 0x4000;
+	const DWORD MAX_DMG = 0x4000;
+	const DWORD MAX_THRUST = 0x4000;
 	const int MAX_CASH = 999999999;
 
 	// Open process
@@ -37,24 +37,24 @@ int main()
 	uintptr_t dynamicPtrBaseAddress = moduleBase + 0x11D9AC;
 
 	// Get memory addresses
-	uintptr_t playerThrustAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, playerThrustOffsets);
-	uintptr_t playerShieldAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, playerShieldOffsets);
-	uintptr_t playerCashAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, playerCashOffsets);
-	uintptr_t playerStallWarningLevelAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, playerStallWarningLvlOffsets);
-	uintptr_t playerStatusAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, playerStatusOffsets);
-	uintptr_t currentTargetStructureDmgAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, currentTargetStructureDmgOffsets);
-	uintptr_t currentTargetTypeAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, currentTargetTypeOffsets);
+	uintptr_t thrustAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, thrustOffsets);
+	uintptr_t shieldAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, shieldOffsets);
+	uintptr_t cashAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, cashOffsets);
+	uintptr_t stallWarningLevelAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, stallWarningLvlOffsets);
+	uintptr_t statusAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, statusOffsets);
+	uintptr_t targetStructureDmgAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, targetStructureDmgOffsets);
+	uintptr_t targetTypeAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, targetTypeOffsets);
 
 	// Game variables
-	int newThrust = 150000;
-	int stallWarningLvl;
-	int newStallWarningLvl = 0;
-	int currentTargetStructureDmg;
-	int currentTargetType;
-	int currentShields;
-	int playerCash;
+	DWORD newThrust = MAX_THRUST + 0x249F0;
+	DWORD targetStructureDmg;
+	DWORD shields;
 	BYTE playerStatus = 0;
 	BYTE newPlayerStatus = 0;
+	int newStallWarningLvl = 0;
+	int stallWarningLvl;
+	int targetType;
+	int cash;
 
 	system("color A");
 
@@ -68,34 +68,35 @@ int main()
 	}
 
 	while (1) {
-		currentTargetTypeAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, currentTargetTypeOffsets);
-		currentTargetStructureDmgAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, currentTargetStructureDmgOffsets);
-		currentTargetType = ReadProcessMemory(hProcess, (BYTE*)currentTargetTypeAddress, &currentTargetType, sizeof(currentTargetType), nullptr);
+		// Update target info
+		targetTypeAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, targetTypeOffsets);
+		targetStructureDmgAddress = FindDmaAddress(hProcess, dynamicPtrBaseAddress, targetStructureDmgOffsets);
+		targetType = ReadProcessMemory(hProcess, (BYTE*)targetTypeAddress, &targetType, sizeof(targetType), nullptr);
 
 		// Instakill targeted pilot
-		ReadProcessMemory(hProcess, (BYTE*)currentTargetTypeAddress, &currentTargetType, sizeof(currentTargetType), nullptr);
-		ReadProcessMemory(hProcess, (BYTE*)currentTargetStructureDmgAddress, &currentTargetStructureDmg, sizeof(currentTargetStructureDmg), nullptr);
-		if ((currentTargetType == 1) && (currentTargetStructureDmg != MAX_DMG)) {
-			WriteProcessMemory(hProcess, (BYTE*)currentTargetStructureDmgAddress, &MAX_DMG, sizeof(MAX_DMG), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)targetTypeAddress, &targetType, sizeof(targetType), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)targetStructureDmgAddress, &targetStructureDmg, sizeof(targetStructureDmg), nullptr);
+		if ((targetType == 1) && (targetStructureDmg != MAX_DMG)) {
+			WriteProcessMemory(hProcess, (BYTE*)targetStructureDmgAddress, &MAX_DMG, sizeof(MAX_DMG), nullptr);
 			std::cout << "Pilot detected and destroyed" << std::endl;
 		}
 
 		// Invincibility
-		ReadProcessMemory(hProcess, (BYTE*)playerShieldAddress, &currentShields, sizeof(currentShields), nullptr);
-		if (currentShields < MAX_SHIELDS) {
-			WriteProcessMemory(hProcess, (BYTE*)playerShieldAddress, &MAX_SHIELDS, sizeof(MAX_SHIELDS), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)shieldAddress, &shields, sizeof(shields), nullptr);
+		if (shields < MAX_SHIELDS) {
+			WriteProcessMemory(hProcess, (BYTE*)shieldAddress, &MAX_SHIELDS, sizeof(MAX_SHIELDS), nullptr);
 			std::cout << "Damage detected - shields restored to FULL" << std::endl;
 		}
 
 		// Stall negation
-		ReadProcessMemory(hProcess, (BYTE*)playerStallWarningLevelAddress, &stallWarningLvl, sizeof(stallWarningLvl), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)stallWarningLevelAddress, &stallWarningLvl, sizeof(stallWarningLvl), nullptr);
 		if (stallWarningLvl > 0) {
-			WriteProcessMemory(hProcess, (BYTE*)playerStallWarningLevelAddress, &newStallWarningLvl, sizeof(newStallWarningLvl), nullptr);
+			WriteProcessMemory(hProcess, (BYTE*)stallWarningLevelAddress, &newStallWarningLvl, sizeof(newStallWarningLvl), nullptr);
 			std::cout << "Stall detected and averted" << std::endl;
 		}
 
 		// Report player status
-		ReadProcessMemory(hProcess, (BYTE*)playerStatusAddress, &newPlayerStatus, sizeof(newPlayerStatus), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)statusAddress, &newPlayerStatus, sizeof(newPlayerStatus), nullptr);
 		if ((newPlayerStatus == 3) && (newPlayerStatus != playerStatus)) {
 			std::cout << "Player is now in a moth" << std::endl;
 			playerStatus = newPlayerStatus;
@@ -106,7 +107,7 @@ int main()
 		}
 
 		// Speedhack
-		WriteProcessMemory(hProcess, (BYTE*)playerThrustAddress, &newThrust, sizeof(newThrust), nullptr);
+		WriteProcessMemory(hProcess, (BYTE*)thrustAddress, &newThrust, sizeof(newThrust), nullptr);
 
 		// Check if process is still running
 		HWND hwWindow = FindWindow(0, _T("Hardwar"));
@@ -117,9 +118,9 @@ int main()
 		}
 
 		// Infinite cash
-		ReadProcessMemory(hProcess, (BYTE*)playerCashAddress, &playerCash, sizeof(playerCash), nullptr);
-		if (playerCash < MAX_CASH) {
-			WriteProcessMemory(hProcess, (BYTE*)playerCashAddress, &MAX_CASH, sizeof(MAX_CASH), nullptr);
+		ReadProcessMemory(hProcess, (BYTE*)cashAddress, &cash, sizeof(cash), nullptr);
+		if (cash < MAX_CASH) {
+			WriteProcessMemory(hProcess, (BYTE*)cashAddress, &MAX_CASH, sizeof(MAX_CASH), nullptr);
 			std::cout << "Player cash re-maxed" << std::endl;
 		}
 
